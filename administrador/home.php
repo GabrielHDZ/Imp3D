6a2d1457867e13b8b88f4039b4991280a9c020d2
GabrielHDZ <Gabrielhdz0102@gmail.com>
2022-03-01 19:01:46 -0600

    update 1 march

--- administrador/home.php
+++ administrador/home.php
@@ -1,3 +1,11 @@
 <?php include('templates/header.php') ?>
+<section>
+    <article>
+<h1>Formulario de ingreso de datos</h1>
+    </article>
+    <article>
+        <h1>Tabla de presentaciond de informacion</h1>
+    </article>
+</section>
 
 <?php include('templates/footer.php')?>
